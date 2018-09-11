fccea117dc23a33ccfc7e8a5ff886e4f7f5a0aa1
gunnarsireus <gunnar.sireus@gmail.com>
2018-09-11 17:55:10 +0200

    Added Bookshelf.cs

---
+++ Api/Models/Bookshelf.cs
@@ -0,0 +1,47 @@
+﻿using Api.DAL;
+using Api.Data;
+using Api.Models;
+using System.Collections.Generic;
+
+namespace Api.Services
+{
+    public class Bookshelf : IBookshelf
+    {
+        private readonly BookUnitOfWork _unitOfWork;
+        public Bookshelf(IApiContext context)
+        {
+            _unitOfWork = new BookUnitOfWork(context);
+        }
+
+        private IEnumerable<Book> books;
+
+        public IEnumerable<Book> Books
+        {
+            get { return _unitOfWork.Books.GetAll(); }
+            set { books = value; }
+        }
+
+        public bool Loan(Book book)
+        {
+            var oldBook = _unitOfWork.Books.Get(book.Id);
+            if (oldBook == null) return false;
+            oldBook = book;
+            oldBook.Disabled = false;
+            oldBook.InShelf = false;
+            _unitOfWork.Books.Update(oldBook); //LoanedTo has to be set earlier
+            return true;
+        }
+
+        public bool Return(Book book)
+        {
+            var oldBook = _unitOfWork.Books.Get(book.Id);
+            if (oldBook == null) return false;
+            oldBook = book;
+            oldBook.Disabled = false;
+            oldBook.InShelf = true;
+            oldBook.LoanedTo = "";
+            _unitOfWork.Books.Update(oldBook);
+            return true;
+        }
+    }
+}
---
+++ Api/Models/IBookshelf.cs
@@ -0,0 +1,12 @@
+﻿using Api.Models;
+using System.Collections.Generic;
+
+namespace Api.Services
+{
+    interface IBookshelf
+    {
+        IEnumerable<Book> Books { get; }
+        bool Loan(Book book);
+        bool Return(Book book);
+    }
+}
